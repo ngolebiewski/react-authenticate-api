f48821d6d46ebbb0f6728c7a1c9bb8be253e7a19
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-11-17 17:12:02 -0500

    readme updates

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -5,25 +5,39 @@ const signUpForm = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(`I'm handleSubmit and asyncronous`);
+    try {
+   
+        const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            username: username, 
+            password: password})
+        });
+        const data = await response.json();
+        console.log(data);
+
+      
+    } catch (error) {
+      setError(error.message);
+    }
   }
 
   return (
     <>
       <h2>Sign Up</h2>
+
+      {error && <p>{error}</p>}
+
       <form onSubmit={handleSubmit}>
         <label>Username: <input value={username} name="username" type="text"
-        onChange={e => {
-          setUsername(e.target.value)
-          console.log(e.target.value)}
-        }
+          onChange={e => setUsername(e.target.value)
+          }
         /></label><br></br>
-        <label>Password: <input value={password} name="password" type="text" 
-        onChange={e => {
-          setPassword(e.target.value)
-          console.log(e.target.value)}}
+        <label>Password: <input value={password} name="password" type="text"
+          onChange={e => setPassword(e.target.value)}
         /></label><br></br>
         <button value="submit">Submit</button>
       </form>
